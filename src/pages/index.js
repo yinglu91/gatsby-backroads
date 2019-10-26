9679f926288b8bbbea2cb77e6697e9700c61eb58
Ying Lu <yinglu91@gmail.com>
2019-10-25 20:53:43 -0400

    finished index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
-
+import SimpleHero from '../components/SimpleHero'
+import Banner from '../components/Banner'
+import About from '../components/Home/About'
+import Services from '../components/Home/Services'
 export default () => (
     <Layout>
-      Hello from backroad!
+      <SimpleHero>
+        <Banner title="contine exploring" info="sdfsajf sdfas sfasdf sdfasdf sdfsa sdfs sfs">
+          <Link to="/tours" className="btn-white">explore tours</Link>
+        </Banner>
+         
+      </SimpleHero>
+      <About />
+      <Services />
     </Layout>
 
 )
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -3,6 +3,8 @@ import Layout from '../components/Layout'
 
 export default class tours extends Component {
   render() {
-    return <Layout>hello from tours page!!!!</Layout>
+    return <Layout>hello from tours page!!!!
+      
+    </Layout>
   }
 }
