6ec4c5f146ed2d84284a9346a1a99111b9ffe0ef
Ying Lu <yinglu91@gmail.com>
2019-10-27 13:22:49 -0400

    added AniLink and contact email formspree

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
     `gatsby-transformer-sharp`, 
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`, 
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-transition-link`,
   ],
 }
