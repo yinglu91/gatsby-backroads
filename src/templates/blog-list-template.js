bf0e7040ce56951e54c9c61c2966dac18f491385
Ying Lu <yinglu91@gmail.com>
2019-11-09 08:55:05 -0500

    setup post paginator

---
+++ src/templates/blog-list-template.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import styles from '../css/blog.module.css'
+import BlogCard from '../components/Blog/BlogCard'
+import Title from '../components/Title'
+
+const BlogListTemplate = () => {
+  return <div>This is blogs template</div>
+}
+
+export const query = graphql``
+
+export default BlogListTemplate
