36f599a0aa32e08866469653b9ef9e8ca9857c3e
Ying Lu <yinglu91@gmail.com>
2019-11-09 13:02:03 -0500

    added paginator  number

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -6,10 +6,66 @@ import styles from '../css/blog.module.css'
 import BlogCard from '../components/Blog/BlogCard'
 import Title from '../components/Title'
 
-const BlogListTemplate = () => {
-  return <div>This is blogs template</div>
+const BlogListTemplate = props => {
+  console.log(props)
+  const { currentPages, numPages } = props.pageContext
+  const { data } = props
+
+  return (
+    <Layout>
+      <section className={styles.blog}>
+        <Title title="latest" subtitle="posts" />
+        <div className={styles.center}>
+          {data.posts.edges.map(({ node }) => {
+            return <BlogCard key={node.id} blog={node} />
+          })}
+        </div>
+
+        <section className={styles.links}>
+          {Array.from({ length: numPages }, (_, i) => {
+            return (
+              <AniLink
+                key={i}
+                fade
+                to={`/blogs/${i === 0 ? '' : i + 1}`}
+                className={
+                  i + 1 === currentPages
+                    ? `${styles.link} ${styles.active}`
+                    : `${styles.link}`
+                }
+              >
+                {i + 1}
+              </AniLink>
+            )
+          })}
+        </section>
+      </section>
+    </Layout>
+  )
 }
 
-export const query = graphql``
+export const query = graphql`
+  query getPosts($skip: Int!, $limit: Int!) {
+    posts: allContentfulPost(
+      skip: $skip
+      limit: $limit
+      sort: { fields: published, order: DESC }
+    ) {
+      edges {
+        node {
+          slug
+          title
+          id: contentful_id
+          published(formatString: "MMMM Do, YYYY")
+          image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default BlogListTemplate
